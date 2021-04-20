579c136661c16735171e0231f489ea7c5e70aae8
piyush <piyush.khandelwal@oodlestechnologies.com>
2021-04-20 19:02:18 +0530

    Initial Commit

---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+DEV_STATIC_VARS = {
+    'db_settings': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'booking_mgmt',
+        'USER': 'premier_agent_network',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_VARS = DEV_STATIC_VARS
